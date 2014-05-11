2a58db6a546be0c5445f8ae3f6a958a2682521fd
stw <Mestway@gmail.com>
2014-05-11 08:02:04 -0700

    May 11th.

--- output/testDummy.java
+++ output/testDummy.java
@@ -1,8 +1,12 @@
-class NoP {public NoP() { }};
+class NoP {public NoP(int a0,int a1) { }};
 class P {
     private P() { super(); }
     
-    static P newInstance() { return new P(); }
+    static P newInstance(int a, int b) {
+        a++;
+        b--;
+        return new P();
+    }
     
     Q1 newQ1() { return this.new Q1(); }
     
@@ -24,7 +28,7 @@ public class testDummy {
     static void print(java.lang.Object ... x) { for (java.lang.Object e : x) java.lang.System.out.println(e + " "); }
     
     public static void main(java.lang.String[] args) {
-        NoP x = new NoP();
+        NoP x = new NoP(7,3);
         Q y = new Q(); }
     
     public testDummy() { super(); }
